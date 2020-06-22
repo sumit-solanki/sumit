3d392bed41834add322d26d3cfb53e6b0b4267d2
sumit <sumit@sixwallz.com>
2020-06-22 12:44:55 +0530

    save file

--- Sivcan Singh_files/text-autotype.js
+++ Sivcan Singh_files/text-autotype.js
@@ -1,6 +1,6 @@
 var i = 1;
 var j= -1;
-var list = ['UI/UX Developer | Full Stack Web Developer | Programmer | iOS Developer'];
+var list = ['UI/UX Developer | FRONTEND Developer | Programmer '];
 var a = list[0];
 var len = list.length;
 var num = 0;
